8476ca807711390a375d2ed3eeff5d9ff6ccb69c
hotate1000 <ooo.336.sss@gmail.com>
2023-03-16 21:22:34 +0900

    モデルを作成する

--- application/flask_blog/__init__.py
+++ application/flask_blog/__init__.py
@@ -1,7 +1,12 @@
 # Flaskのインポート
 from flask import Flask;
+# SQLAlchemyライブラリをインストールする
+from flask_sqlalchemy import SQLAlchemy
 # Flaskアプリケーションを作成
 app = Flask(__name__);
 # configファイルの位置を設定
 app.config.from_object('flask_blog.config');
+# ｋの変数を参照することでデータベースを扱うことが出来る
+db = SQLAlchemy(app);
+
 import flask_blog.views
