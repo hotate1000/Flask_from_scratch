013f96b7ea47d68f66e04f6470988d781f5a1150
hotate1000 <ooo.336.sss@gmail.com>
2023-03-16 18:53:39 +0900

    リンクに関するコードのリファクタリング

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -3,9 +3,9 @@ from flask import request, redirect, url_for, render_template, flash, session
 
 @app.route('/')
 def show_entries():
-    # return "Hello World!";
     if not session.get('logged_in'):
-        return redirect('/login');
+        # return redirect('/login');
+        return redirect(url_for('login'));
     return render_template('entries/index.html');
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -20,11 +20,14 @@ def login():
         else:
             session['logged_in'] = True;
             flash('ログインしました');
-            return redirect('/');
+            # return redirect('/');
+            return redirect(url_for('show_entries'));
     return render_template('login.html');
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None);
     flash('ログアウトしました');
-    return redirect('/');
+    # return redirect('/');
+    return redirect(url_for('show_entries'));
+
