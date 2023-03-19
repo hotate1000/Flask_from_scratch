35678208a4c1a50d485ed0fd86f0e8a54bbe6a93
hotate1000 <ooo.336.sss@gmail.com>
2023-03-19 22:45:03 +0900

    コードを綺麗にする

--- application/manage.py
+++ application/manage.py
@@ -1,6 +1,6 @@
-from flask_script import Manager
-from flask_blog import app
-from flask_blog.scripts.db import InitDB
+from flask_script import Manager;
+from flask_blog import app;
+from flask_blog.scripts.db import InitDB;
 
 if __name__ == "__main__":
     manager = Manager(app);
